58bbbb91168110a89c5d9f9ccce18177e5b91c70
satwinderbhullar <satwinderbhullar@10.51.206.187>
2019-09-12 10:50:55 -0400

    Tesing Git first time!

---
+++ Test/TestCase.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCase {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		System.out.println("helllo");
+	}
+
+}
